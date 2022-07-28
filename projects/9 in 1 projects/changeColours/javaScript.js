6b8b50939397a9695d03eb8d313d5d23ecb82df2
Lavanya Gopathi <lavanya.bete@gmail.com>
2022-07-28 23:33:31 +0100

    commit all the pushes

--- projects/9 in 1 projects/changeColours/javaScript.js
+++ projects/9 in 1 projects/changeColours/javaScript.js
@@ -1,18 +1,14 @@
-const hexNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
-const hexBtn = document.querySelector(".hexBtn");
+const rgbBtn = document.querySelector(".rgbBtn");
 const bodyBcg = document.querySelector('body');
-const hex = document.querySelector('.hex');
-hexBtn.addEventListener('click', getHex);
+const rgbSelector = document.querySelector('.rgbSelector');
+rgbBtn.addEventListener('click', getRgb);
 
-function getHex() {
-    let hexCol = '#';
-    for (let i = 0; i < 6; i++) {
-        let random = Math.floor(Math.random() * hexNumbers.length);
-        console.log(random);
-        hexCol += hexNumbers[random];
-        console.log(hexCol);
-    }
-    bodyBcg.style.backgroundColor = hexCol;
-    hex.innerHTML = hexCol;
+function getRgb() {
+    let redCol = Math.floor(Math.random() * 256);
+    let greenCol = Math.floor(Math.random() * 256);
+    let blueCol = Math.floor(Math.random() * 256);
+    let rgbCol = "rgb(" + redCol + "," + greenCol + "," + blueCol + ")";
 
+    bodyBcg.style.backgroundColor = rgbCol;
+    rgbSelector.innerHTML = rgbCol;
 }
