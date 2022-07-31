e57dff8a852c749137c7bfb48e1049ca1f1b17ea
Lavanya Gopathi <lavanya.bete@gmail.com>
2022-08-01 00:24:10 +0100

    add all the updates

---
+++ projects/9 in 1 projects/ImageSlider/imageSlider.js
@@ -0,0 +1,29 @@
+const prevBtn = document.querySelector('.prevBtn');
+const nextBtn = document.querySelector('.nextBtn');
+const container = document.querySelector(".images");
+
+let counter = 0;
+
+nextBtn.addEventListener("click", nextSlide);
+prevBtn.addEventListener('click', prevSlide);
+
+function nextSlide() {
+    container.animate([{ opacity: '0.1' }, { opacity: '1.0' }], { duration: 1000, fill: 'forwards' });
+    if (counter === 4) {
+        counter = -1;
+    }
+
+    counter++;
+
+    container.style.backgroundImage = 'url(images/bcg-${counter}.jpg)';
+}
+
+function prevSlide() {
+    if (counter === 0) {
+        counter = 5;
+    }
+
+    counter--;
+
+    container.style.backgroundImage = 'url(images/bcg-${counter}.jpg)';
+}
