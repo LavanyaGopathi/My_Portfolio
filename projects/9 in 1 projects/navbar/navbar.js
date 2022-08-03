4b919af94572db59b9c1acddc104a86e26a58834
Lavanya Gopathi <lavanya.bete@gmail.com>
2022-08-03 23:13:20 +0100

    add all the updates

--- projects/9 in 1 projects/navbar/navbar.js
+++ projects/9 in 1 projects/navbar/navbar.js
@@ -2,12 +2,12 @@ const navbarBtn = document.querySelector('.navbar_btn')
 const navbarLinks = document.querySelector('.navbar_links')
 
 navbarBtn.addEventListener('click', function() {
-    let value = navbarLinks.classList.contains('navbar_collapase')
+    let value = navbarLinks.classList.contains('navbar__collapse')
     if (value) {
-        navbarLinks.classList.remove('navbar_collapase')
+        navbarLinks.classList.remove('navbar__collapse')
         navbarBtn.classList.remove('change')
     } else {
-        navbarLinks.classList.add('navbar_collapse')
+        navbarLinks.classList.add('navbar__collapse')
         navbarBtn.classList.add('change')
     }
 })
